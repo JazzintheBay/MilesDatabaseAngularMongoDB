6a976168ed4dd334501b7be5dc77d0c4ccac091d
ahnafy <yasin011@morris.umn.edu>
2018-12-09 19:07:59 -0600

    add scheme for show object to the json parser

--- server/src/main/java/umm3601/Shows/ShowsCollectionImporter.java
+++ server/src/main/java/umm3601/Shows/ShowsCollectionImporter.java
@@ -26,9 +26,133 @@ public class ShowsCollectionImporter {
             DBCollection collection = db.getCollection("Shows");
 
             // convert JSON to DBObject directly
-            // currently only adds object to collection Specified
+            // currently only adds one object to the shows collection
+            // Things to work on: Work on Multiple objects parsing through
+            // Create import for parse from Json File and not manually hardcode it in
+            // for new imports to work everytime
             DBObject dbObject = (DBObject) JSON
-                    .parse("{'event':'JazzInTheBay', 'EventID':'Forever'}");
+                    .parse("{{\n" +
+                            "  \"EventGroup\": {\n" +
+                            "    \"EventGroupID\": {},\n" +
+                            "    \"Event\": [\n" +
+                            "      {\n" +
+                            "        \"EventID\": {},\n" +
+                            "        \"EventName\": {},\n" +
+                            "        \"Headline\": {},\n" +
+                            "        \"MusicFestival\": {\n" +
+                            "          \"FestivalID\": {},\n" +
+                            "          \"FestivalName\": {},\n" +
+                            "          \"Year\": {},\n" +
+                            "          \"Image\": [],\n" +
+                            "          \"doorTime\": {},\n" +
+                            "          \"startTime\": {},\n" +
+                            "          \"endTime\": {}\n" +
+                            "        },\n" +
+                            "        \"Performer\": {\n" +
+                            "          \"MusicGroup\": [\n" +
+                            "            {\n" +
+                            "              \"MusicGroupID\": {},\n" +
+                            "              \"MusicGroupName\": {},\n" +
+                            "              \"Weblinks\": {\n" +
+                            "                \"URLType\": {},\n" +
+                            "                \"URL\": {}\n" +
+                            "              },\n" +
+                            "              \"MusicCategory\": [],\n" +
+                            "              \"MusicGroupType\": [],\n" +
+                            "              \"MusicGroupTags\": [],\n" +
+                            "              \"GroupLead\": [\n" +
+                            "                {\n" +
+                            "                  \"ArtistID\": {},\n" +
+                            "                  \"ArtistName\": {},\n" +
+                            "                  \"ArtistRole\": [],\n" +
+                            "                  \"Instruments\": [],\n" +
+                            "                  \"Awards\": [\n" +
+                            "                    {\n" +
+                            "                      \"Type\": {},\n" +
+                            "                      \"AwardName\": {},\n" +
+                            "                      \"AwardCategory\": {},\n" +
+                            "                      \"Year\": {}\n" +
+                            "                    }\n" +
+                            "                  ]\n" +
+                            "                }\n" +
+                            "              ],\n" +
+                            "              \"MusicGroupMember\": [\n" +
+                            "                {\n" +
+                            "                  \"ArtistID\": {},\n" +
+                            "                  \"ArtistName\": {},\n" +
+                            "                  \"ArtistRole\": [],\n" +
+                            "                  \"Instruments\": [],\n" +
+                            "                  \"Awards\": [\n" +
+                            "                    {\n" +
+                            "                      \"Type\": {},\n" +
+                            "                      \"AwardName\": {},\n" +
+                            "                      \"AwardCategory\": {},\n" +
+                            "                      \"Year\": {}\n" +
+                            "                    }\n" +
+                            "                  ]\n" +
+                            "                }\n" +
+                            "              ]\n" +
+                            "            }\n" +
+                            "          ],\n" +
+                            "          \"Awards\": [\n" +
+                            "            {\n" +
+                            "              \"Type(award or nomination)\": {},\n" +
+                            "              \"AwardName\": {},\n" +
+                            "              \"AwardCategory\": {},\n" +
+                            "              \"Year\": {}\n" +
+                            "            }\n" +
+                            "          ],\n" +
+                            "          \"Review\": [\n" +
+                            "            {\n" +
+                            "              \"Excerpt\": {},\n" +
+                            "              \"Reviewer\": {},\n" +
+                            "              \"URL\": {}\n" +
+                            "            }\n" +
+                            "          ]\n" +
+                            "        },\n" +
+                            "        \"Location\": {\n" +
+                            "          \"VenueID\": {},\n" +
+                            "          \"VenueName\": {},\n" +
+                            "          \"VenueTags(array)\": {},\n" +
+                            "          \"Weblinks\": {\n" +
+                            "            \"URL\": {}\n" +
+                            "          },\n" +
+                            "          \"Address\": {\n" +
+                            "            \"Street\": {},\n" +
+                            "            \"Building\": {},\n" +
+                            "            \"City\": {},\n" +
+                            "            \"State\": {},\n" +
+                            "            \"ZipCode\": {},\n" +
+                            "            \"Country\": {},\n" +
+                            "            \"MapsURL\": []\n" +
+                            "          }\n" +
+                            "        },\n" +
+                            "        \"Ticket\": {\n" +
+                            "          \"TicketURL\": {},\n" +
+                            "          \"TicketPrice\": {\n" +
+                            "            \"LowPrice\": {},\n" +
+                            "            \"HighPrice\": {},\n" +
+                            "            \"DiscountTags\": []\n" +
+                            "          }\n" +
+                            "        },\n" +
+                            "        \"EventSourceURL\": {},\n" +
+                            "        \"ShowcaseContent\": {\n" +
+                            "          \"ContentType\": {},\n" +
+                            "          \"ContentURL\": {}\n" +
+                            "        },\n" +
+                            "        \"EventDescription\": {},\n" +
+                            "        \"EventAlbum\": [\n" +
+                            "          {\n" +
+                            "            \"AlbumID\": {},\n" +
+                            "            \"AlbumName\": {},\n" +
+                            "            \"AlbumURL\": {}\n" +
+                            "          }\n" +
+                            "        ]\n" +
+                            "      }\n" +
+                            "    ]\n" +
+                            "  },\n" +
+                            "  \"URLType(Website/FB/Spotify etc.)\": {}\n" +
+                            "}}");
 
             collection.insert(dbObject);
 
